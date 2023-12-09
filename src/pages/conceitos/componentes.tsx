d7660a82ee4a2a15c6f821194d8bc4d2fa63ca01
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-08 22:55:10 -0300

    inserir componentes

---
+++ src/pages/conceitos/componentes.tsx
@@ -0,0 +1,15 @@
+export default function Componentes() {
+  return (
+    <div className="flex justify-center items-center h-screen gap-4">
+      <button className="btn-info">
+        Info
+      </button>
+      <button className="btn-success">
+        Sucesso
+      </button>
+      <button className="btn-error">
+        Erro
+      </button>
+    </div>
+  );
+}
