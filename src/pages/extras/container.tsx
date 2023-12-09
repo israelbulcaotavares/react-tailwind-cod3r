8df8250b6d02e62f9919fd2d954b1d4b6b45b582
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-09 12:39:41 -0300

    container

---
+++ src/pages/extras/container.tsx
@@ -0,0 +1,17 @@
+export default function Container() {
+  return (
+    <div>
+      <div className="container mx-auto text-4xl h-48 my-10 bg-red-500">
+        Container 1
+      </div>
+
+      <div className="container mx-auto text-4xl h-48 my-10 bg-red-500">
+        Container 2
+      </div>
+      
+      <div className="container mx-auto text-4xl h-48 my-10 bg-red-500">
+        Container 3
+      </div>
+    </div>
+  );
+}
