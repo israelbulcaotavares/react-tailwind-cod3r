ef17d7b7ae027a8f7e92d823f77cf54448f9cef4
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-08 22:21:51 -0300

    mobile

---
+++ src/pages/conceitos/mobile.tsx
@@ -0,0 +1,9 @@
+export default function Mobile() {
+  // "x"
+  return (
+    <div className="flex justify-center items-center bg-red-500 h-48 text-3xl md:text-4xl lg:text-6xl w-[90%] 2xl:w-[70]">
+      <span className="inline">Mobile</span>
+      <span className="hidden">Não mobile</span>
+    </div>
+  );
+}
