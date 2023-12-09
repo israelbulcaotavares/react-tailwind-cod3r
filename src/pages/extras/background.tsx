3386fc38f28a96c34a5c5918d13da6789def181e
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-09 12:59:54 -0300

    background gradiente

---
+++ src/pages/extras/background.tsx
@@ -0,0 +1,21 @@
+export default function Background() {
+  return (
+    <div>
+      {/* <div className="flex justify-center items-center h-screen bg-gradient-to-r from-slate-600">
+        Background
+      </div> */}
+      {/* <div className="flex justify-center items-center h-screen bg-gradient-to-r from-purple-800 via-purple-950 to-black">
+        Background
+      </div> */}
+      {/* <div className="flex justify-center items-center h-screen bg-gradient-to-br from-purple-800 via-purple-950 to-black">
+        Background
+      </div> */}
+      {/* <div className="flex justify-center items-center h-screen bg-gradient-to-l from-purple-800 via-purple-950 to-black">
+        Background
+      </div> */}
+      <div className="flex justify-center items-center h-screen bg-gradient-to-tl from-purple-900 via-black to-purple-900">
+        Background
+      </div>
+    </div>
+  );
+}
