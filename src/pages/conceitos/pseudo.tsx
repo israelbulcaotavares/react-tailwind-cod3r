d30024e1f285f012e12f2c354b0908cc23237040
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-08 22:21:35 -0300

    classes e pseudoclasses

---
+++ src/pages/conceitos/pseudo.tsx
@@ -0,0 +1,9 @@
+export default function Pseudo() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button className="p-5 rounded-md text-5xl hover:bg-green-300 hover:text-red-400 bg-green-500 ">
+        Salvar
+      </button>
+    </div>
+  );
+}
