8ddd3779bb553ca6e462f4b73ddeebefeb56d05c
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-11 16:04:47 -0300

    navegação

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 interface MenuItemProps {
@@ -8,11 +9,13 @@ interface MenuItemProps {
 
 export default function MenuItem(props: MenuItemProps) {
   return (
+    <Link href={props.url}>
     <div className="flex  items-center gap-2  py-2 px-3 rounded-md text-zinc-400 hover:bg-black">
       <span>{React.cloneElement(props.icone, {
             stroke:1 /* afinar os icones */
       })}</span>
       <span className="text-sm">{props.texto}</span>
-    </div>
+    </div> 
+    </Link>
   );
 }
