d2a0c578a906312377d9c0d265cf9d92688c63cb
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-11 16:14:10 -0300

    ajustes layout

--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -8,25 +8,25 @@ interface PaginaProps {
 }
 
 export default function Pagina(props: PaginaProps) {
-    const usuario = {
-        nome:'Frankie Sullivan',
-        email:'frankiesullivan@zmail.com',
-        imagemUrl: 'https://img.freepik.com/vetores-premium/design-de-avatar-de-pessoa_24877-38131.jpg'
-        
-    }
+  const usuario = {
+    nome: "Frankie Sullivan",
+    email: "frankiesullivan@zmail.com",
+    imagemUrl:
+      "https://img.freepik.com/vetores-premium/design-de-avatar-de-pessoa_24877-38131.jpg",
+  };
   return (
     <div className="flex h-screen">
-      <aside className="flex flex-col bg-zinc-900 w-72">
-        <Logo className="p-7"/>
-        <Menu className="p-7" />
-        <hr className="mx-7 border-zinc-700"/>
-        <UsuarioInfo {...usuario} className="p-7"/>
+      <aside className="flex flex-col bg-zinc-900 w-80">
+        <Logo className="p-7 px-7" />
+        <Menu className="p-7 overflow-auto pb-28" />
+        <div className="fixed bottom-0 bg-zinc-900 w-80">
+          <hr className="mx-7 border-zinc-700" />
+          <UsuarioInfo {...usuario} className="p-7" />
+        </div>
       </aside>
       <div className="p-7">
         <div>{props.children}</div>
-       
       </div>
-      
     </div>
   );
 }
