10129719648456ad3385fdff6336128e383176ba
Israel Tavares <israelbulcaotavares@gmail.com>
2023-12-11 15:38:23 -0300

    inserir usuario info

--- src/components/template/Pagina.tsx
+++ src/components/template/Pagina.tsx
@@ -1,16 +1,30 @@
-import Menu from "./Menu"
+import { IconDotsVertical } from "@tabler/icons-react";
+import Menu from "./Menu";
+import UsuarioInfo from "./UsuarioInfo";
 
-interface PaginaProps{
-    children: any
+interface PaginaProps {
+  children: any;
 }
 
-export default function Pagina(props: PaginaProps){
-    return(
-        <div className="flex h-screen">
-            <Menu className="bg-zinc-800 w-72" />
-            <div className="p-7">
-            <div>{props.children}</div>
-        </div>
-        </div>
-    )
+export default function Pagina(props: PaginaProps) {
+    const usuario = {
+        nome:'Frankie Sullivan',
+        email:'frankiesullivan@zmail.com',
+        imagemUrl: 'https://img.freepik.com/vetores-premium/design-de-avatar-de-pessoa_24877-38131.jpg'
+        
+    }
+  return (
+    <div className="flex h-screen">
+      <aside className="flex flex-col bg-zinc-900 w-72">
+        <Menu className="p-7" />
+        <hr className="mx-7 border-zinc-700"/>
+        <UsuarioInfo {...usuario} className="p-7"/>
+      </aside>
+      <div className="p-7">
+        <div>{props.children}</div>
+       
+      </div>
+      
+    </div>
+  );
 }
---
+++ src/components/template/UsuarioInfo.tsx
@@ -0,0 +1,28 @@
+import { IconDotsVertical } from "@tabler/icons-react";
+import Image from "next/image";
+
+interface UsuarioInfoProps {
+  nome: string;
+  email: string;
+  imagemUrl: string;
+  className?: string;
+}
+
+export default function UsuarioInfo(props: UsuarioInfoProps) {
+  return (
+    <div className={`flex gap-2 ${props.className ?? ""}`}>
+      <Image
+        src={props.imagemUrl}
+        alt="avatar"
+        height={40}
+        width={40}
+        className="rounded-full"
+      />
+      <div className="flex flex-col">
+        <span>{props.nome}</span>
+        <span className="text-sm text-zinc-400">{props.email}</span>
+      </div>
+      <IconDotsVertical className="cursor-pointer" />
+    </div>
+  );
+}
